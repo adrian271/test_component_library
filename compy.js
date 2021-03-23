00ba032a461b80a301f10ef483d3013b4a6cbc7a
Adrian Barnes <adrian271@gmail.com>
2021-03-23 02:02:19 -0700

    add compy

---
+++ compy.js
@@ -0,0 +1,7 @@
+const Compy = ({ siteTitle }) => (
+  <div>
+    <button>this is a button</button>
+  </div>
+);
+
+module.exports.Compy = Compy;
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function imgShadow(options) {
   let images = document.querySelectorAll('.imgShadow');
 
-  if (options.shadow_type === 'hard') options.shadow_type = 'Opx';
-  else options.shadow_typ = '15px';
+  if (options.shadow_type === 'hard') {
+    options.shadow_type = 'Opx';
+  } else {
+    options.shadow_type = '15px';
+  }
 
   images.forEach((image) => {
     image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,.2)`;
